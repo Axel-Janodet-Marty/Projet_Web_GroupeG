a0f4350ebfa33429102810fcc100a498a2e7f3e1
Skander Hamdouni <148259016+Skander-Hamdouni@users.noreply.github.com>
2025-05-14 11:07:17 +0200

    Update script.js

--- Coraux/js/script.js
+++ Coraux/js/script.js
@@ -1,1 +1,10 @@
+console.log("Bienvenue sur le site dédié aux coraux !");
 
+window.addEventListener("DOMContentLoaded", () => {
+    const titles = document.querySelectorAll("h2");
+    titles.forEach(title => {
+        title.addEventListener("click", () => {
+            title.style.color = "#ff6600";
+        });
+    });
+});
